ef0575c8c2012904e6b3b393573415e59098757d
Swadeep Dhondi <dhondiswadeepdsp6@gmail.com>
2024-10-22 00:59:35 +0530

    adding frontend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workroutes = require("./routes/workouts");
+const cors = require("cors");
 
 //express app
 const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+//components
+import WorkoutDetials from "../components/WorkoutDetails";
+
 const Home = () => {
+  const [workouts, setworkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:4000/api/workouts");
+      const json = await response.json();
+      if (response.ok) {
+        setworkouts(json);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>HOME</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetials key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
